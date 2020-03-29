d9e70b88e141440fc6f8306266549cb913509e67
thexdev <akbarnugroho253@gmail.com>
2020-03-30 01:21:54 +0700

    created navigation bar component

---
+++ src/components/navbar/index.js
@@ -0,0 +1,31 @@
+import React, { memo } from "react";
+import { Link } from "react-router-dom";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+const Navigationbar = () => {
+  return (
+    <Navbar variant="dark" expand="lg">
+      <Container>
+        <Link to="/" className="navbar-brand">
+          Kepo Corona
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Link to="/" className="nav-link mx-2 text-dark">
+              Beranda
+            </Link>
+            <Link to="/news" className="nav-link mx-2 text-dark">
+              Berita
+            </Link>
+            <Link to="/about" className="nav-link mx-2 text-dark">
+              Tentang
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default memo(Navigationbar);
